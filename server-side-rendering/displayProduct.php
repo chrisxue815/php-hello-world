6e995bc875a2f2086df6dce5210a32a0587c9bea
Chris Xue <chrisxue815@gmail.com>
2019-11-23 20:31:18 +0000

    Clean up

--- server-side-rendering/displayProduct.php
+++ server-side-rendering/displayProduct.php
@@ -6,6 +6,6 @@ $productLine = $_POST['orderNumber'] + 2;
 ?>
 
 <div>
-Product code: <input type="text" id="productCode" readonly value="<?php echo $productCode ?>" />
-Product line: <input type="text" id="productLine" readonly value="<?php echo $productLine ?>" />
+Product code: <input type="text" id="productCode" disabled value="<?php echo $productCode ?>" />
+Product line: <input type="text" id="productLine" disabled value="<?php echo $productLine ?>" />
 </div>
